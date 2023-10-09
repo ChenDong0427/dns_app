757eb3f4231ae7a0a8422eff54643c6ba7126d14
Chen Dong <chendong@chens-mbp.mynetworksettings.com>
2023-10-09 18:27:44 -0400

    initial commit

---
+++ FS/FS.py
@@ -0,0 +1,54 @@
+# Author: Chen Dong
+# DCN 2023 Fall 
+# Lab 3
+from flask import Flask, abort, Response, request
+from socket import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def initialize():
+    return "Initialization Succeed"
+
+@app.route('/register', methods = ["PUT"], endpoint="register")
+def fs():
+
+    hostname = request.json.get("hostname")
+    ip = request.json.get("ip")
+    as_ip = request.json.get("as_ip")
+    as_port = request.json.get("as_port")
+
+    dnsMessage = "TYPE=A\n" + "NAME=" + hostname + "\nVALUE=" + ip + "\nTTL=10"
+    address = (ip, 53533)
+
+    fibonacciSocket = socket(AF_INET, SOCK_DGRAM)
+    fibonacciSocket.sendto(dnsMessage.encode(), address)
+
+    response, addr = fibonacciSocket.recvfrom(2048)
+    print(response.decode())
+
+    return Response("Registration Finished!", status=201)
+
+@app.route('/fibonacci', methods = ["GET"], endpoint="fibonacci")
+def fs():
+    arg = request.args
+    if not arg.get("number"):
+        abort(400, "Parameter Missing!")
+    num = arg.get("number")
+
+    for character in num:
+        if character not in [str(i) for i in range(10)]:
+            abort(400, "Wrong Parameter Type!")
+    num = int(num)
+
+    def fib(n):
+        a, b = 1, 1
+        for i in range(n - 1):
+            a, b = b, a + b
+        return a
+
+    return str(fib(num))
+
+app.run(host = '0.0.0.0',
+        port = 9090,
+        debug = True)
